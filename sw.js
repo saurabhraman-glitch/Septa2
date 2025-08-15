ec167ccdc1ee06f24f9df41fefad3f636d60c06b
saurabhraman-glitch <saurabh.raman@gmail.com>
2025-08-14 22:06:39 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,7 @@
+
+const CACHE='septa-pwa-v1';
+const SHELL=['.','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE&&caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{const u=new URL(e.request.url);if(u.pathname.startsWith('/.netlify/functions/'))return;
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
